21122ddc5352b41c931c1d346f0d0bae4458d2de
deadlydragon00 <elmarrhex.gomez@gmail.com>
2015-06-13 16:31:34 +0800

    added sample project

--- bitmapcropper/src/main/java/io/github/bitmapcropper/Cropper.java
+++ bitmapcropper/src/main/java/io/github/bitmapcropper/Cropper.java
@@ -8,6 +8,7 @@ import android.media.FaceDetector;
 public class Cropper {
 
     public static void getImage() {
+
         FaceDetector.Face[] a = new FaceDetector.Face[10];
 
         FaceDetector face = new FaceDetector(500, 500, 10);
