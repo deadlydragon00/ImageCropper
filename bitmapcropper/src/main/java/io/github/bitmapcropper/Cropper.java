8443e38b60defa0b3e97152d1c8fd65c7f44a6ee
deadlydragon00 <elmarrhex.gomez@gmail.com>
2015-06-13 16:09:25 +0800

    first commit

---
+++ bitmapcropper/src/main/java/io/github/bitmapcropper/Cropper.java
@@ -0,0 +1,17 @@
+package io.github.bitmapcropper;
+
+import android.media.FaceDetector;
+
+/**
+ * Created by deadlydragon00 on 6/13/2015.
+ */
+public class Cropper {
+
+    public static void getImage() {
+        FaceDetector.Face[] a = new FaceDetector.Face[10];
+
+        FaceDetector face = new FaceDetector(500, 500, 10);
+
+    }
+
+}
