a597963ff1de34d1c7ce73c9166efd723fe471c1
deadlydragon00 <elmarrhex.gomez@gmail.com>
2015-06-16 19:10:08 +0800

    evaluate false postive of a certain area

--- bitmapcropper/src/main/java/io/github/bitmapcropper/haar/HaarCascade.java
+++ bitmapcropper/src/main/java/io/github/bitmapcropper/haar/HaarCascade.java
@@ -17,13 +17,27 @@ public class HaarCascade {
     /**
      * Analyze the given portion of image whether is pass the cascade
      *
-     * @param image   the portion of the image
-     * @param cascade the cascade template
+     * @param image       the portion of the image
+     * @param cascade     the cascade template
+     * @param imageWidth  the image width
+     * @param imageHeight the image height
      * @return true if it has a cascade feature
      */
-    public static boolean cascadePass(int[] image, byte[][] cascade) {
+    public static boolean cascadePass(int[] image, byte[][] cascade, int imageWidth, int imageHeight) {
+        if (imageWidth < cascade[0].length || imageHeight < cascade.length)
+            throw new IllegalArgumentException("Cascade must not be bigger than image portion");
 
-        return false;
+        int sum = 0;
+        for (int x = 0; x < imageHeight; x++) {
+            for (int y = 0; y < imageHeight; y++) {
+                if (image[x * y] == cascade[x][y]) {
+                    sum++;
+                }
+            }
+        }
+
+        // count the image pixel and get the 3/4 value at compare to the sum
+        return image.length * 3 / 4 >= sum ? true : false;
     }
 
 
